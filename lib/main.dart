50de81da7828985aeee0ace700be7f693ea9f0f1
AayushPandeyy <ap200061@gmail.com>
2024-08-15 14:40:56 +0545

    Login Screen created

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:game_rating_app/providers/GameProvider.dart';
 import 'package:game_rating_app/providers/RatingProvider.dart';
 import 'package:game_rating_app/screens/HomePage.dart';
 import 'package:game_rating_app/screens/MainPage.dart';
+import 'package:game_rating_app/screens/auth/SignInScreen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainPage(),
+      home: const SignInScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
